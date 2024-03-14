62e4e690394799441daf75f877fdb94b09da3423
Saksham Awasthi <sakshamawasthi1111@gmail.com>
2024-03-15 00:39:08 +0530

    removed

--- variable/variables.tf
+++ variable/variables.tf
@@ -3,14 +3,14 @@ variable "aws_access_key" {
   description = "AWS Access Key"
   type        = string
   sensitive   = true
-  default = AKIAZQ3DQ4GWU6N36OOS
+  default = 
 }
 
 variable "aws_secret_key" {
   description = "AWS Secret Key"
   type        = string
   sensitive   = true
-  default = ttTWQKkbKJJMQAQ2Ufktf6OPqu27wMfMp+Rpig8G
+  default = 
 
 }
 
