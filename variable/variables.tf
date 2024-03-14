5d5051424078d1f6a90e2846e65017ef6cf21da7
Saksham Awasthi <sakshamawasthi1111@gmail.com>
2024-03-15 00:33:04 +0530

    Added a new bucket name

---
+++ variable/variables.tf
@@ -0,0 +1,22 @@
+
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+  default = AKIAZQ3DQ4GWU6N36OOS
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true
+  default = ttTWQKkbKJJMQAQ2Ufktf6OPqu27wMfMp+Rpig8G
+
+}
+
+variable "region" {
+  description = "The AWS region to deploy resources (Input)"
+  type        = string
+  default     = "us-east-1"
+  }
+
